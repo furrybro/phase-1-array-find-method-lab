ac11aabe2d3635e8aa6561120e6cee6ba5fb3970
Jeena Park <jeenapark15@gmail.com>
2022-05-02 10:13:15 -0400

    commit tree 03d9b3ffae6ecc163bc56099776d7ef0ca080398

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// function superbowlWin(records) {
+//     for (const record of records) {
+//         if (record.result === "W") {
+//             return record.year;
+//         }
+//     }
+// }
+
+function superbowlWin(records) {
+    const winFinder = (element) => {return element.result === "W"}
+    const wins = records.find(winFinder)
+    if (wins === undefined) {
+        return undefined;
+    } 
+    return wins.year;
+}
